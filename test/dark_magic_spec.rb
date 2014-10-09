7c0a8ad3ae85e616fb10faddbe18ac0e3ab74ec9
Vinay Bhat <vinaybhat001@gmail.com>
2014-10-09 18:34:00 +0530

    test files

--- test/dark_magic_spec.rb
+++ test/dark_magic_spec.rb
@@ -3,4 +3,5 @@ require_relative '../lib/userinput'
 describe UserInput, "basic input" do
   it "should take in input"
   it "should show hard coded output"
+  it "should iterate for every block"
 end
